70d6f8fc19b2048344370e8add74ddec2eef873d
Sichile Malonje <s.malonje@gmail.com>
2026-03-03 20:29:04 +0200

    deleted unnecessary files

--- app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  res.cookies.set("aw_session", "", { httpOnly: true, path: "/", maxAge: 0 });
-  return res;
-}
